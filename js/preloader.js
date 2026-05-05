01edd4a44370b3904dbdf5ea12de88df43427677
VoDuyNghia <46209046+VoDuyNghia@users.noreply.github.com>
2026-05-05 14:02:38 +0700

    8888

--- js/preloader.js
+++
@@ -1,6 +0,0 @@
- // preloader
- const preloader  = document.getElementById('preloader')
- document.addEventListener("DOMContentLoaded",setTimeout(function pre() {
- preloader.classList.add('uk-hidden')
- 
- },500) );
